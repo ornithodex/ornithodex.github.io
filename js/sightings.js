caae51781c068911ffece8b584a85f93c3ce471a
IlliterateLexicon <lexi.yates@protonmail.com>
2023-01-24 10:24:15 -0700

    Added test Card, Worked on layout

---
+++ js/sightings.js
@@ -0,0 +1,23 @@
+{
+	"sightings": [
+	{
+		"com-name": "Black-Billed Magpie",
+		"sci-name": "Pica hudsonia",
+		"viewer-name": "Lexi Y",
+		"date": "Jan. 24, 2023",
+		"time": "12:33pm",
+		"county": "Utah",
+		"img": "https://www.allaboutbirds.org/guide/assets/photo/302389511-720px.jpg"
+	},
+	
+	{
+		"com-name": "Black-Billed Magpie",
+		"sci-name": "Pica hudsonia",
+		"viewer-name": "Lexi Y",
+		"date": "Jan. 24, 2023",
+		"time": "12:33pm",
+		"county": "Utah",
+		"img": "https://www.allaboutbirds.org/guide/assets/photo/302389511-720px.jpg"
+	}
+]
+}
