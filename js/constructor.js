4cc5436c78daa68215c2a03de703d21eb0ac2202
IlliterateLexicon <lexi.yates@protonmail.com>
2023-02-16 01:11:33 -0700

    updated

--- js/constructor.js
+++ js/constructor.js
@@ -1,10 +1,11 @@
 var request = new XMLHttpRequest(); 
-var requestUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT7peqmhRImLAk7GjWZvTJQ8gEz3CegWXD9OfHCRCfv9muRI6U-A49xMl7RZllMDEIdgOEfA7YVOaif/pub?gid=1549752026&single=true&output=csv"
+var requestUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT7peqmhRImLAk7GjWZvTJQ8gEz3CegWXD9OfHCRCfv9muRI6U-A49xMl7RZllMDEIdgOEfA7YVOaif/pub?gid=343623131&single=true&output=csv"
 
 request.onload = function() { 
 	var text = this.responseText;
-	console.log(text);
-	return text;
+	let array = text.split(", ");
+	console.info(array);
+	return array;
 } 
 
 request.open("get", requestUrl, true); 
