941d951f6245084313271f213b525d9069c045d6
IlliterateLexicon <lexi.yates@protonmail.com>
2023-02-16 09:17:08 -0700

    changed test entries falg and shifted catagories

--- js/constructor.js
+++ js/constructor.js
@@ -1,6 +1,6 @@
 // THE TEST ENTRIES CAN BE SHOW OR DISPLAYED BY CHANGING THIS FLAG
 // INPUT "__test__" as "Your Name" IN THE FORM TO FLAG A ENTRY AS A TEST
-var displayTestEntries = false;
+var displayTestEntries = true;
 
 var request = new XMLHttpRequest();
 var requestUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT7peqmhRImLAk7GjWZvTJQ8gEz3CegWXD9OfHCRCfv9muRI6U-A49xMl7RZllMDEIdgOEfA7YVOaif/pub?gid=343623131&single=true&output=csv"
